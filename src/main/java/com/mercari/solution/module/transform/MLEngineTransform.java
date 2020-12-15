3d6adab977633cd77b9801e7d7fe8d16aa6aadce
orfeon <orfeon@mercari.com>
2020-12-15 10:27:57 +0900

    sdk version up.

--- src/main/java/com/mercari/solution/module/transform/MLEngineTransform.java
+++ src/main/java/com/mercari/solution/module/transform/MLEngineTransform.java
@@ -1,31 +1,8 @@
 package com.mercari.solution.module.transform;
 
-import com.google.gson.Gson;
-import com.squareup.okhttp.*;
-
-import java.io.IOException;
-import java.util.List;
 
 public class MLEngineTransform {
 
-    private Gson gson = new Gson();
-    private List<Object> buffer;
-    private OkHttpClient client;
 
-    private class Prediction {
-
-    }
-
-    private Prediction sendRequest() throws IOException {
-        String json = this.gson.toJson(this.buffer);
-        Response response = this.client
-                .newCall(new Request.Builder()
-                        .post(RequestBody.create(
-                                MediaType.parse("application/json"),
-                                json))
-                        .build()
-                ).execute();
-        return this.gson.fromJson(response.body().charStream(), Prediction.class);
-    }
 
 }
