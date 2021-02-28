7fc971126a6a5ebd689649fe457b0c95c9d47918
Yoshitaro Makise <yoshizow@turtlewalk.org>
2021-02-28 23:38:18 +0900

    Automatically discover builtin modules

--- src/main/java/com/mercari/solution/module/ModuleRegistry.java
+++ src/main/java/com/mercari/solution/module/ModuleRegistry.java
@@ -1,13 +1,19 @@
 package com.mercari.solution.module;
 
-import com.mercari.solution.module.sink.*;
-import com.mercari.solution.module.source.*;
-import com.mercari.solution.module.transform.*;
+import com.google.common.reflect.ClassPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ModuleRegistry {
+    private static final Logger LOG = LoggerFactory.getLogger(ModuleRegistry.class);
+
     private static final ModuleRegistry instance = new ModuleRegistry();
 
     private final Map<String, SourceModule> sources = new HashMap<>();
@@ -23,44 +29,26 @@ public class ModuleRegistry {
     }
 
     private void registerBuiltinModules() {
-        registerSource(new BigQuerySource());
-        registerSource(new DatastoreSource());
-        registerSource(new JdbcSource());
-        registerSource(new PubSubSource());
-        registerSource(new SpannerBackupSource());
-        registerSource(new SpannerSource());
-        registerSource(new StorageSource());
+        List<Class<? extends SourceModule>> sourceModules = findModulesInPackage("com.mercari.solution.module.source", SourceModule.class);
+        sourceModules.forEach(this::registerSource);
 
-        registerTransform(new AutoMLTablesTransform());
-        registerTransform(new BeamSQLTransform());
-        registerTransform(new FeatureTransform());
-        registerTransform(new FlattenTransform());
-        registerTransform(new GroupByTransform());
-        registerTransform(new PDFExtractTransform());
-        registerTransform(new ReshuffleTransform());
-        registerTransform(new SetOperationTransform());
-        registerTransform(new WindowTransform());
+        List<Class<? extends TransformModule>> transformModules = findModulesInPackage("com.mercari.solution.module.transform", TransformModule.class);
+        transformModules.forEach(this::registerTransform);
 
-        registerSink(new BigQuerySink());
-        registerSink(new BigtableSink());
-        registerSink(new DatastoreSink());
-        registerSink(new JdbcSink());
-        registerSink(new SolrIndexSink());
-        registerSink(new SpannerSink());
-        registerSink(new StorageSink());
-        registerSink(new TextSink());
+        List<Class<? extends SinkModule>> sinkModules = findModulesInPackage("com.mercari.solution.module.sink", SinkModule.class);
+        sinkModules.forEach(this::registerSink);
     }
 
-    public void registerSource(SourceModule module) {
-        sources.put(module.getName(), module);
+    public void registerSource(Class<? extends SourceModule> moduleClass) {
+        registerModule(sources, moduleClass);
     }
 
-    public void registerTransform(TransformModule module) {
-        transforms.put(module.getName(), module);
+    public void registerTransform(Class<? extends TransformModule> moduleClass) {
+        registerModule(transforms, moduleClass);
     }
 
-    public void registerSink(SinkModule module) {
-        sinks.put(module.getName(), module);
+    public void registerSink(Class<? extends SinkModule> moduleClass) {
+        registerModule(sinks, moduleClass);
     }
 
     public SourceModule getSource(String name) {
@@ -74,4 +62,36 @@ public class ModuleRegistry {
     public SinkModule getSink(String name) {
         return sinks.get(name);
     }
+
+    private <T extends Module> List<Class<? extends T>> findModulesInPackage(String packageName, Class<T> moduleClass) {
+        ClassPath classPath;
+        try {
+            ClassLoader loader = getClass().getClassLoader();
+            classPath = ClassPath.from(loader);
+        } catch (IOException ioe) {
+            throw new RuntimeException("Reading classpath resource failed", ioe);
+        }
+        List<Class<? extends T>> modules = classPath.getTopLevelClassesRecursive(packageName)
+                .stream()
+                .map(ClassPath.ClassInfo::load)
+                .filter(moduleClass::isAssignableFrom)
+                .map(clazz -> (Class<? extends T>)clazz.asSubclass(moduleClass))
+                .collect(Collectors.toList());
+        return modules;
+    }
+
+    private <T extends Module> void registerModule(Map<String, T> container, Class<? extends T> moduleClass) {
+        T module;
+        try {
+            module = moduleClass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            throw new RuntimeException("Failed to instantiate module: " + moduleClass, e);
+        }
+        if (container.containsKey(module.getName())) {
+            throw new IllegalStateException("Module is already registered: " + module.getName());
+        }
+        container.put(module.getName(), module);
+        LOG.debug("registered module: " + module.getName());
+    }
+
 }
