4d88dc18c32001d18f1410206e07758096d8d906
orfeon <orfeon@mercari.com>
2023-02-01 16:49:40 +0900

    fix filter type conversion bug

--- src/main/java/com/mercari/solution/util/Filter.java
+++ src/main/java/com/mercari/solution/util/Filter.java
@@ -140,14 +140,7 @@ public class Filter implements Serializable {
                 if(!child.isJsonObject()) {
                     throw new IllegalArgumentException("Simple conditions must be jsonObject. json: " + child.toString());
                 }
-                JsonObject childObject = child.getAsJsonObject();
-                if(!childObject.has("key") || !childObject.has("op") || !childObject.has("value")) {
-                    throw new IllegalArgumentException("Simple conditions must contain `key`,`op`,`value`. json: " + child.toString());
-                }
-                final ConditionLeaf leaf = new ConditionLeaf();
-                leaf.setKey(childObject.get("key").getAsString());
-                leaf.setOp(Op.of(childObject.get("op").getAsString()));
-                leaf.setValue(childObject.get("value"));
+                final ConditionLeaf leaf = createLeaf(child.getAsJsonObject());
                 leaves.add(leaf);
             }
             ConditionNode node = new ConditionNode();
@@ -162,12 +155,17 @@ public class Filter implements Serializable {
 
     public static ConditionNode parse(final JsonObject jsonObject) {
         if(!jsonObject.has("and") && !jsonObject.has("or")) {
-            throw new IllegalArgumentException("Condition must contain `and` or `or`. Condition json: " + jsonObject.toString());
-        }
-
-        if(jsonObject.has("and") && jsonObject.has("or")) {
+            final List<ConditionLeaf> leaves = new ArrayList<>();
+            final ConditionLeaf leaf = createLeaf(jsonObject);
+            leaves.add(leaf);
+            ConditionNode node = new ConditionNode();
+            node.setType(Type.AND);
+            node.setLeaves(leaves);
+            return node;
+        } else if(jsonObject.has("and") && jsonObject.has("or")) {
             throw new IllegalArgumentException("Condition must contain only one of `and` or `or`. Condition json: " + jsonObject.toString());
         }
+
         final Type type = jsonObject.has("and") ? Type.AND : Type.OR;
         final JsonElement conditions = jsonObject.has("and") ? jsonObject.get("and") : jsonObject.get("or");
         if(!conditions.isJsonArray()) {
@@ -181,13 +179,7 @@ public class Filter implements Serializable {
                 final ConditionNode node = parse(child);
                 nodes.add(node);
             } else {
-                if(!child.has("key") || !child.has("op") || !child.has("value")) {
-                    throw new IllegalArgumentException("Condition leaf must contain `key`, `op`, `value` parameter. Condition leaf json: " + child.toString());
-                }
-                final ConditionLeaf leaf = new ConditionLeaf();
-                leaf.setKey(child.get("key").getAsString());
-                leaf.setOp(Op.of(child.get("op").getAsString()));
-                leaf.setValue(child.get("value"));
+                final ConditionLeaf leaf = createLeaf(child);
                 leaves.add(leaf);
             }
         }
@@ -243,6 +235,28 @@ public class Filter implements Serializable {
         return is(condition.getType(), bits);
     }
 
+    public static boolean filter(final ConditionNode condition, final Map<String, Object> values) {
+        final List<Boolean> bits = new ArrayList<>();
+
+        if(condition.getLeaves() != null && condition.getLeaves().size() > 0) {
+            for(ConditionLeaf leaf : condition.getLeaves()) {
+                final Object value = Optional.ofNullable(values.get(leaf.getKey())).orElse(null);
+                bits.add(is(value, leaf));
+            }
+        }
+        if(condition.getNodes() != null && condition.getNodes().size() > 0) {
+            for(ConditionNode node : condition.getNodes()) {
+                bits.add(filter(node, values));
+            }
+        }
+
+        if(bits.size() == 0) {
+            return false;
+        }
+
+        return is(condition.getType(), bits);
+    }
+
     public static boolean is(final Type type, final Collection<Boolean> bits) {
         if(type.equals(Type.AND)) {
             return bits.stream().allMatch(v -> v);
@@ -275,16 +289,22 @@ public class Filter implements Serializable {
             return leaf.getOp().equals(Op.NOT_IN);
         } else {
             final int c;
+
             if(value instanceof Byte
-                    || value instanceof Short
-                    || value instanceof Integer
-                    || value instanceof Long
-                    || value instanceof Float
-                    || value instanceof Double
                     || value instanceof BigInteger
                     || value instanceof BigDecimal) {
 
                 c = new BigDecimal(value.toString()).compareTo(leaf.getValue().getAsBigDecimal());
+            } else if(value instanceof Short) {
+                c = ((Short)value).compareTo(leaf.getValue().getAsShort());
+            } else if(value instanceof Integer) {
+                c = ((Integer)value).compareTo(leaf.getValue().getAsInt());
+            } else if(value instanceof Long) {
+                c = ((Long)value).compareTo(leaf.getValue().getAsLong());
+            } else if(value instanceof Float) {
+                c = ((Float)value).compareTo(leaf.getValue().getAsFloat());
+            } else if(value instanceof Double) {
+                c = ((Double)value).compareTo(leaf.getValue().getAsDouble());
             } else if(value instanceof String) {
                 c = ((String)value).compareTo(leaf.getValue().getAsString());
             } else if(value instanceof Instant) {
@@ -293,8 +313,11 @@ public class Filter implements Serializable {
                 c = ((LocalDate)value).compareTo(DateTimeUtil.toLocalDate(leaf.getValue().getAsString()));
             } else if(value instanceof LocalTime) {
                 c = ((LocalTime)value).compareTo(DateTimeUtil.toLocalTime(leaf.getValue().getAsString()));
+            } else if(value instanceof org.apache.avro.util.Utf8) {
+                c = ((org.apache.avro.util.Utf8)value).toString().compareTo(leaf.getValue().getAsString());
             } else {
-                throw new IllegalArgumentException("Condition compare op must be Number or String. : " + value.getClass());
+                c = (value).toString().compareTo(leaf.getValue().getAsString());
+                //throw new IllegalArgumentException("Condition compare op must be Number or String. : " + value.getClass());
             }
 
             switch (leaf.getOp()) {
@@ -318,6 +341,17 @@ public class Filter implements Serializable {
                     throw new IllegalArgumentException("");
             }
         }
+    }
+
+    private static ConditionLeaf createLeaf(final JsonObject jsonObject) {
+        if(!jsonObject.has("key") || !jsonObject.has("op") || !jsonObject.has("value")) {
+            throw new IllegalArgumentException("Simple conditions must contain `key`,`op`,`value`. json: " + jsonObject);
+        }
+        final ConditionLeaf leaf = new ConditionLeaf();
+        leaf.setKey(jsonObject.get("key").getAsString());
+        leaf.setOp(Op.of(jsonObject.get("op").getAsString()));
+        leaf.setValue(jsonObject.get("value"));
+        return leaf;
     }
 
     public interface Getter<T> extends Serializable {
